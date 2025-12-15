5dc0d97585a730b88295437c1bce5dcc7a9a27ee
Jason Swett <jason@benfranklinlabs.com>
2025-12-15 17:43:06 -0500

    Remove dead code: Client class and unused DockerRegistryCache methods.

--- lib/saturn_ci_worker_api/docker_registry_cache.rb
+++ lib/saturn_ci_worker_api/docker_registry_cache.rb
@@ -24,19 +24,5 @@ module SaturnCIWorkerAPI
       system("echo '#{@password}' | docker login #{URL} -u #{@username} --password-stdin")
       $CHILD_STATUS.success?
     end
-
-    def pull_image
-      output = `docker pull #{image_url} 2>&1`
-
-      if output.include?('not found') || output.include?('manifest unknown')
-        "Docker registry cache miss. Image not found in registry: #{image_url}"
-      else
-        'Docker registry cache hit'
-      end
-    end
-
-    def push_image
-      system("docker push #{image_url}")
-    end
   end
 end
